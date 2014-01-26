ed6e85b8d4f1b28db29acaff633d9387ae1e1296
Aleksandar Diklic <rastasheep3@gmail.com>
2014-01-26 23:04:45 +0100

    Added error handling

--- javascripts/app.js
+++ javascripts/app.js
@@ -10,10 +10,22 @@ app.config(function($routeProvider) {
       templateUrl : "projects.html",
       controller  : "projectsController"
     });
-
 });
 
-app.controller("mainController", function($scope, $location, tokenService) {
+app.run(function($rootScope){
+  $rootScope.alerts = [];
+
+  $rootScope.addAlert = function(type, message) {
+    $rootScope.alerts.push({type: type, msg: message});
+  };
+
+  $rootScope.closeAlert = function(index) {
+    $rootScope.alerts.splice(index, 1);
+  };
+})
+
+app.controller("mainController", function($scope, $rootScope, $location, tokenService) {
+
   var init = function() {
     tokenService.getToken().then( function(value){
       if (value){
@@ -29,37 +41,56 @@ app.controller("mainController", function($scope, $location, tokenService) {
     return true
   };
 
+  $scope.getAlerts = function() {
+    return $rootScope.alerts;
+  };
+
+  $scope.closeAlert = function(index) {
+    $rootScope.closeAlert();
+  };
+
   init();
 });
 
-app.controller("projectsController", function($scope, $http, $location, tokenService){
+app.controller("projectsController", function($rootScope, $scope, $http, $location, tokenService){
   $scope.working = true;
 
-  tokenService.getToken()
-    .then( function(value){ $scope.token = value })
-    .then( function(value){
-      url = "https://semaphoreapp.com/api/v1/projects?auth_token=" + $scope.token
-      $http.get(url)
-           .success(function(data) {
-             $scope.projects = data;
-             $scope.working = false;
-           })
-           .error(function(data, status, headers, config) {
-             tokenService.removeToken()
-             $location.path("/");
-           });
-    });
+  var init = function() {
+    getProjects()
+  }
 
-    $scope.morePages = true;
-    var pagesShown = 1;
-    var pageSize = 5;
-    $scope.pageLimit = function() {
-      return pageSize * pagesShown;
-    };
-    $scope.nextPage = function() {
-      pagesShown = pagesShown + 1;
-      $scope.morePages = pagesShown < ($scope.projects.length / pageSize);
-    };
+  var getProjects = function(){
+    tokenService.getToken()
+      .then( function(value){ $scope.token = value })
+      .then( function(value){
+        url = "https://semaphoreapp.com/api/v1/projects?auth_token=" + $scope.token
+        $http.get(url)
+          .success(function(data) {
+            $scope.projects = data;
+            $scope.working = false;
+         })
+          .error(function(data, status, headers, config) {
+            tokenService.removeToken();
+            $rootScope.addAlert("danger", "Wrong token, please try again.");
+            $location.path("/");
+          });
+      });
+   }
+
+  $scope.morePages = true;
+  var pagesShown = 1;
+  var pageSize = 5;
+
+  $scope.pageLimit = function() {
+    return pageSize * pagesShown;
+  };
+
+  $scope.nextPage = function() {
+    pagesShown = pagesShown + 1;
+    $scope.morePages = pagesShown < ($scope.projects.length / pageSize);
+  };
+
+  init();
 })
 
 app.service("tokenService", function ($q) {
